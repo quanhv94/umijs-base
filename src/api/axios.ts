cc1bfbc19f4150620e73b1bc43d01d9cc73a2aef
QuanHV <quanhv94@gmail.com>
2020-04-29 08:44:34 +0700

    update

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -26,13 +26,13 @@ axiosInstance.interceptors.response.use(
   error => Promise.reject(error),
 );
 
-export const sendGet = (url: string, params: any) =>
+export const sendGet = (url: string, params?: any) =>
   axiosInstance.get(url, { params });
-export const sendPost = (url: string, params: any, queryParams: any) =>
+export const sendPost = (url: string, params?: any, queryParams?: any) =>
   axiosInstance.post(url, params, { params: queryParams });
-export const sendPut = (url: string, params: any) =>
+export const sendPut = (url: string, params?: any) =>
   axiosInstance.put(url, params);
-export const sendPatch = (url: string, params: any) =>
+export const sendPatch = (url: string, params?: any) =>
   axiosInstance.patch(url, params);
-export const sendDelete = (url: string, params: any) =>
+export const sendDelete = (url: string, params?: any) =>
   axiosInstance.delete(url, { params });
