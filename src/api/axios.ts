9bf8846724b7ee328853b5ea820ba42610a9f9e6
QuanHV <quanhv94@gmail.com>
2020-06-23 11:53:32 +0700

    axios refresh token

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,6 @@
 import Axios from 'axios';
 import Cookies from 'js-cookie';
+import { history } from 'umi';
 import configs from '../config/config';
 
 const axiosInstance = Axios.create({
@@ -14,16 +15,51 @@ axiosInstance.interceptors.request.use(
   },
   error => Promise.reject(error),
 );
-axiosInstance.interceptors.response.use(
-  response => {
-    // invalid token
-    // if (typeof (response.data) !== 'object') {
-    //   Cookies.remove('token');
-    //   history.push('/');
-    // }
-    return response;
+
+axiosInstance.interceptors.request.use(
+  config => {
+    // eslint-disable-next-line no-param-reassign
+    config.headers.Authorization = `Bearer ${Cookies.get('token')}`;
+    config.headers.timezoneoffset = new Date().getTimezoneOffset();
+    return config;
   },
   error => Promise.reject(error),
+);
+axiosInstance.interceptors.response.use(
+  response => response,
+  error => {
+    const originalConfig = error.config;
+    if (error.response.status !== 401) {
+      return Promise.reject(error);
+    }
+    const refreshToken = Cookies.get('refreshToken');
+    if (!refreshToken) {
+      Cookies.remove('token');
+      history.push('/');
+      return;
+    }
+    return Axios.post(`${configs.API_DOMAIN}/v1/web/auth/refresh`, {
+      refreshToken,
+    })
+      .then(res => {
+        if (res.status === 200) {
+          const data = res.data;
+          Cookies.set('token', data.token);
+          Cookies.set('refreshToken', data.refreshToken);
+          originalConfig.headers.Authorization = `Bearer ${data.token}`;
+          return Axios(originalConfig);
+        } else {
+          Cookies.remove('token');
+          Cookies.remove('refreshToken');
+          history.push('/');
+        }
+      })
+      .catch(() => {
+        Cookies.remove('token');
+        Cookies.remove('refreshToken');
+        history.push('/');
+      });
+  },
 );
 
 export const sendGet = (url: string, params?: any) =>
